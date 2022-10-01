3fd9a9e692e97065eb93c9b12005c5feba000854
parellano6 <palomasemailis@gmail.com>
2022-10-01 19:32:55 -0400

    avg_grade

--- avg_grade_arellanp.py
+++ avg_grade_arellanp.py
@@ -1,11 +1,12 @@
 """
 Author: Paloma Arellano, arellanp@purdue.edu
-Assignment: 00.1 - Hello User
+Assignment: 04.3 - Avg Grade
 Date: 08/24/2022
 
 Description:
-    This program asks the user for their name, and then displays
-    a greeting using their name.
+    This program asks the user to enter a valid score five times. The program then displays
+    a letter grade after each score is entered. After all the scores are entered, it displays the
+    average of the scores and the letter grade corresponding to that average.
 
 Contributors:
     Name, login@purdue.edu [repeat for each]
@@ -28,15 +29,51 @@ Academic Integrity Statement:
 """
 
 """Import additional modules below this line (starting with unit 6)."""
-
+from statistics import mean
 
 """Write new functions below this line (starting with unit 4)."""
 
 
 def main():
-    message = input("What is your name? ")
-    print("Hello " + message + "!")
+    score = []              # score array
+    for i in range(5):      # iterage through score
+        score.append(get_valid_score())     # adds values to score
+        grade_calc = determine_grade(score[i])  # calculates grade
+        print("  The letter grade for " + "{:.1f}".format(score[i]) + " is " + grade_calc + ".")
+    
+    avg = calc_average(score)       # average grade calc
+    grade = determine_grade(avg)    # letter grade
+    print("")
+    print("Results:")
+    print("  The average score is " + "{:.2f}".format(avg) + ".")
+    print("  The letter grade for " + "{:.2f}".format(avg) + " is " + grade + ".")
 
+def get_valid_score():
+    val = 101       # intial val
+    while val > 100 or val < 0:     # checks val 
+        val = float(input("Enter a score: "))   # asks for input
+        if val > 100 or val < 0:
+            print("  Invalid Input. Please try again.")
+    return float(val)
+    
+def calc_average(val):
+    average = float(mean(val))      # gets average
+    return average
+    
+def determine_grade(val):
+    if val >= 92:   # A
+        letter = 'A'
+    elif val >= 82: # B
+        letter = 'B'
+    elif val >= 73: # C
+        letter = 'C'
+    elif val >= 64: # D
+        letter = 'D'
+    else:           # F
+        letter = 'F'
+        
+    return letter
+    
 
 """Do not change anything below this line."""
 if __name__ == "__main__":
