8173adb6047624c3b63c9c5753d4a2e7e6c16362
parellano6 <palomasemailis@gmail.com>
2022-11-14 00:11:46 -0500

    finished chapter 9

--- Chapter 9/file_analysis_arellanp.py
+++ Chapter 9/file_analysis_arellanp.py
@@ -35,45 +35,75 @@ from collections import OrderedDict
 
 
 def main():
-    first = {}
+    first = {}          # first dictionary
     with open('python_1.txt', 'r') as file:           # opens file
         for line in file:
             for word in line.split():
                 val = (word.strip()).lower()
                 inVal = ''
                 for i in val:
-                    if (i.isalpha()) or i == '-':
+                    if (i.isalpha()) or i == '-' or i == "'" or i == '/':       # values to look out for
                         inVal += i
+                if inVal == 'https//wwwpythonorg/':
+                    inVal = "https://www.python.org"
+                if inVal == "walls'":
+                    inVal = "walls"
                 if inVal in first.keys():
                     first[inVal] = first.get(inVal) + 1        # adds to dictionary
                 else:
                     first[inVal] = 1
-    first = OrderedDict(sorted(first.items()))
+    first = OrderedDict(sorted(first.items()))      # orders dictionary
                     
-    print(first)
+    #print(first)
     with open("python_1_word_frequency.txt", 'w') as file:
         for key, val in first.items():
             file.write('%s: %s\n' % (key, val))
             
-    second = {}
+    second = {}         # second dictionary
     with open('python_2.txt', 'r') as file:           # opens file
         for line in file:
             for word in line.split():
                 val = (word.strip()).lower()
                 inVal = ''
                 for i in val:
-                    if (i.isalpha()) or i == '-':
+                    if (i.isalpha()) or i == '-' or i == "'" or i == '/' or i == "1":   # values to look out for
                         inVal += i
-                if inVal in first.keys():
+                if inVal == 'https://www.python.org/':
+                    inVal = "https://www.python.org"
+                if inVal == "walls'":
+                    inVal = "walls"
+                if inVal == "c/c/java":
+                    inVal = "c/c++/java"
+                if inVal in second.keys():
                     second[inVal] = second.get(inVal) + 1        # adds to dictionary
                 else:
                     second[inVal] = 1
-    second = OrderedDict(sorted(second.items()))
+    second = OrderedDict(sorted(second.items()))        # orders dictionary
                     
-    print(second)
+    #print(second)
     with open("python_2_word_frequency.txt", 'w') as file:
-        for key, val in second.items():
-            file.write('%s: %s\n' % (key, val))
+        for key, val in second.items():     # iterates through dictionary
+            file.write('%s: %s\n' % (key, val))     # writes values in both files
+            
+    notIn = {}      # not in dictionary
+    with open("common_words.txt", 'w') as file:
+        for firstKey in first:      # iterates through dictionary
+            #print(firstKey)
+            if firstKey in second.keys():
+                file.write(firstKey + "\n")
+            else:
+                notIn[firstKey] = 0
+    
+    for secondKey in second:            # iterates through dictionary
+        if secondKey not in first.keys():
+            notIn[secondKey] = 0
+    
+    notIn = OrderedDict(sorted(notIn.items()))      # orders dictionary
+    #print(notIn)
+    
+    with open("eitherbutnotboth.txt", 'w') as file:
+        for key in notIn:       # iterates through dictionary
+            file.write(key + "\n")
 
 
 """Do not change anything below this line."""
