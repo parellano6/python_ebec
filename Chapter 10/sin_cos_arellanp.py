2858504f98ba2042cb107a0595e6662e02660785
parellano6 <palomasemailis@gmail.com>
2022-11-20 23:53:50 -0500

    finished chapter 10

--- Chapter 10/sin_cos_arellanp.py
+++ Chapter 10/sin_cos_arellanp.py
@@ -1,11 +1,10 @@
 """
 Author: Paloma Arellano, arellanp@purdue.edu
-Assignment: 00.1 - Hello User
-Date: 08/24/2022
+Assignment: 10.4 - Sin Cos
+Date: 11/20/2022
 
 Description:
-    This program asks the user for their name, and then displays
-    a greeting using their name.
+    This program prints out a sine and cosine graph from 0 to 2pi.
 
 Contributors:
     Name, login@purdue.edu [repeat for each]
@@ -28,14 +27,29 @@ Academic Integrity Statement:
 """
 
 """Import additional modules below this line (starting with unit 6)."""
-
+import numpy as np
+import matplotlib.pyplot as plt
 
 """Write new functions below this line (starting with unit 4)."""
 
 
 def main():
-    message = input("What is your name? ")
-    print("Hello " + message + "!")
+    x = np.arange(0, (np.pi * 2) + 0.1, 0.1)        # x range of values
+    ampS = np.sin(x)                                # sin vals
+    ampC = np.cos(x)                                # cos vals
+    
+    fig, ax = plt.subplots()        # plots stuff
+    ax.spines['bottom'].set_position('zero')        # bottom frame moves
+    ax.spines['left'].set_position('zero')          # moves left frame
+    ax.spines['right'].set_color('none')            # deletes right frame
+    ax.spines['top'].set_color('none')              # deletes top frame
+    ax.set_xlim(0, np.pi * 2)                       # sets x limit
+    pi = np.pi
+    plt.xticks(np.arange(pi/2, pi * 2 +pi/2, step=(pi/2)), ['$\\pi/2$', '$\\pi$', '$3\\pi/2$', '$2\\pi$']) # x tick labels
+    plt.yticks([-1, 1])     # y ticks
+    plt.plot(x, ampS, color='red')  # sin
+    plt.plot(x, ampC, color='blue') # cos
+    plt.show()
 
 
 """Do not change anything below this line."""
